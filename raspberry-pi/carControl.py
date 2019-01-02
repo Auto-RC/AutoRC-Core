fef2c8a8de67e0d66546764f0ce65befd9172afc
g-arnav <arnavbrookvale@gmail.com>
2019-01-01 18:45:22 -0800

    removed arduino

--- raspberry-pi/carControl.py
+++ raspberry-pi/carControl.py
@@ -7,17 +7,22 @@ import Adafruit_PCA9685
 class Car_Control:
     def __init__(self, freq=60):
         self.pwm = Adafruit_PCA9685.PCA9685()
-        self.steering_min = 289
+        self.steering_min = 290
         self.steering_max = 492
         self.steering_avg = self.steering_min/2 + self.steering_max/2
+        self.throttle_min = 290
+        self.throttle_max = 400
         self.pwm.set_pwm_freq(freq)
 
     def set_steering(self, position):
-        if -1 < position < 1:
-            position = (position * (self.steering_max-self.steering_avg)) + self.steering_avg
+        if -1 <= position <= 1:
+            position = int((-position * (self.steering_max-self.steering_avg)) + self.steering_avg)
+        print("position: ", position)
         if position > self.steering_max:
             self.pwm.set_pwm(0, 0, self.steering_max)
-        elif position > self.steering_min:
+        elif position < self.steering_min:
             self.pwm.set_pwm(0, 0, self.steering_min)
         else:
             self.pwm.set_pwm(0, 0, position)
+
+    def set_throttle(self, throttle, brake):
--- raspberry-pi/controller_test.py
+++ raspberry-pi/controller_test.py
@@ -1,26 +1,7 @@
-import pygame
-import os
-import time
+from controller import Controller
 
-axes = [3, 4, 2]
-vals = [0] * len(axes)
+controller = Controller()
 
-
-
-pygame.init()
-os.environ["SDL_VIDEODRIVER"] = "dummy"
-pygame.display.init()
-screen = pygame.display.set_mode((1,1))
 while(1):
-    for event in pygame.event.get():
-        pass
-
-    joystick = pygame.joystick.Joystick(0)
-    joystick.init()
-    for i in range(len(axes)):
-        vals[i] = joystick.get_axis(axes[i])
-
-    print("throttle: ", vals[1], "   brake: ", vals[0], "   steering: ", vals[2])
-    time.sleep(1)
-
-
+    controller.update()
+    print(controller.steering, controller.throttle)
