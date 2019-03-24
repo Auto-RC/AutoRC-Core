651f8d03608ee69da46c9e15fb2434c2f77018c3
g-arnav <arnavbrookvale@gmail.com>
2019-03-23 18:05:33 -0700

    added arduino i2c code

--- os/controls/rf/ampullae/ampullae.ino
+++ os/controls/rf/ampullae/ampullae.ino
@@ -1,17 +1,23 @@
 #include <Wire.h>
 
-float THR_MAX = 1925;
-float THR_MIN = 1000;
+// Receiver calibration constants
+float THR_MAX = 1990;
+float THR_MIN = 990;
 float STR_MAX = 1990;
-float STR_MIN = 1050;
-float SWB_MAX = 1900;
-float SWB_MIN = 1000;
-float SWC_MAX = 1000;
-float SWC_MIN = 1000;
-int THR = 1;
-int STR = 2;
-int SWB = 3;
-int SWC = 4;
+float STR_MIN = 990;
+float SWB_MAX = 1990;
+float SWB_MIN = 990;
+float SWC_MAX = 1988;
+float SWC_MIN = 990;
+
+// I2C Format
+byte THR = 0;
+byte STR = 1;
+byte SWB = 2;
+byte SWC = 3;
+int I2C_MAX = 64;
+
+
 
 // These hold the input from the receiver
 int thr_input;
@@ -19,6 +25,7 @@ int str_input;
 int swb_input;
 int swc_input;
 
+// These hold the intermediate values
 float thr;
 float str;
 float swb;
@@ -48,31 +55,46 @@ void loop()
     swb_input = pulseIn(6,HIGH,1000000);
     swc_input = pulseIn(7,HIGH,1000000);
 
-    thr = thr_input - THR_MIN;
-    str = str_input - STR_MIN;
-    swb = swb_input - SWB_MIN;
-    swc = swc_input - SWC_MIN;
-
-    thr_output = ( thr/(THR_MAX-THR_MIN) )*255;
-    str_output = ( str/(STR_MAX-STR_MIN) )*255;
-    swb_output = ( swb/(SWB_MAX-SWB_MIN) )*255;
-    swc_output = ( swc/(SWC_MAX-SWC_MIN) )*255;
+    thr_output = ( (thr_input - THR_MIN)/(THR_MAX-THR_MIN) )*I2C_MAX;
+    str_output = ( (str_input - STR_MIN)/(STR_MAX-STR_MIN) )*I2C_MAX;
+    swb_output = ( (swb_input -SWB_MIN)/(SWB_MAX-SWB_MIN) )*I2C_MAX;
+    swc_output = ( (swc_input -SWC_MIN)/(SWC_MAX-SWC_MIN) )*I2C_MAX;
 
     Serial.print(thr_output);
     Serial.print(" ");
+    Serial.print(thr_input);
+    Serial.print(" ");
     Serial.print(str_output);
     Serial.print(" ");
-    Serial.print(swb_output);
+    Serial.print(str_input);
     Serial.print(" ");
-    Serial.print(swc_output);
-    Serial.print(" ");
+//    Serial.print(swb_output);
+//    Serial.print(" ");
+//    Serial.print(swb_input);
+//    Serial.print(" ");
+//    Serial.print(swc_output);
+//    Serial.print(" ");
+//    Serial.print(swc_input);
+//    Serial.println(" ");
 
+   
+    encode_signal(STR, str_output);
+    
     delay(10);
 }
 
-//void encode_signal(int type, int value)
-//{
-//}
+void encode_signal(int type, byte value)
+{ 
+    byte type_bits = type << 6;
+    byte val_bits = value;
+    byte send_bits = type_bits + val_bits;
+
+    Serial.print(type_bits);
+    Serial.print(" ");
+    Serial.print(val_bits);
+    Serial.print(" ");
+    Serial.println(send_bits);
+}
 //
 //// function that executes whenever data is requested by master
 //// this function is registered as an event, see setup()
