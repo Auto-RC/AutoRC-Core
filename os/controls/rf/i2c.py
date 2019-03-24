d9d64324bb8a3b65eb9bcadfe3290e06fae18b46
g-arnav <arnavbrookvale@gmail.com>
2019-03-23 17:36:35 -0700

    starting ampullaue

--- os/controls/rf/i2c.py
+++ os/controls/rf/i2c.py
@@ -24,11 +24,12 @@ sys.path.append(controls_dir)
 
 from logger import *
 
+
 # ==================================================================================================
-#                                           Ampullae
+#                                           I2C
 # ==================================================================================================
 
-class Ampullae(Thread.thread):
+class I2c:
 
     def __init__(self, address):
 
@@ -36,51 +37,21 @@ class Ampullae(Thread.thread):
         # ------------------------------------------------------------------------------------------
         self.address = address
 
-        self.i2c =
-
-        self.throttle = 0
-        self.steering = 0
-        self.mode = 0
+        self.bus = smbus.SMBus(1)
 
 
     def read(self):
         n = self.bus.read_byte(self.address)
         n_binary = bin(n)[2:]
 
-        if n_binary[0] == 0 and n_binary[1] == 1:
-            self.throttle = int(n[2:], 2)
+        return n_binary
 
-        if n_binary[0] == 1 and n_binary[1] == 0:
-            self.steering = int(n[2:], 2)
-
-        if n_binary[0] == 1 and n_binary[1] == 1:
-            self.mode = int(n[2:], 2)
-
-
-    def get_current_picture(self):
-        return self.cam.run_threaded()
-
-    def stop(self):
-        self.cam.shutdown()
-
-        logger.info("Camera thread ended")
 
 
 # ==================================================================================================
 #                                            TEST CODE
 # ==================================================================================================
 
-if __name__ == '__main__':
-
-    c = Iris(20, (128, 96), 'rgb')
-
-    c.run()
-
-    for i in range(10):
-        print(c.get_current_picture())
-        time.sleep(.5)
-
-    c.stop()
 
 
 
