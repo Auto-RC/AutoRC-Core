430f7b39bca622088cace496a6fa0d963cc4b8eb
g-arnav <arnavbrookvale@gmail.com>
2019-06-29 00:55:12 -0700

    added throttle to cerebellum controller

--- os/controls/cerebellum.py
+++ os/controls/cerebellum.py
@@ -105,9 +105,9 @@ class Cerebellum(threading.Thread):
         self.state['prev_angles'] = self.state['angles']
 
         if 45 < scaled_angle < 65:
-            self.thr = 15
+            self.thr = 20
         else:
-            self.thr = 12
+            self.thr = 15
 
 
     def run(self):
