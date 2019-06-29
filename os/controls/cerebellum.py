b8484cf2697c25ba1b59bf3fb288971a1f7519fb
g-arnav <arnavbrookvale@gmail.com>
2019-06-29 10:49:18 -0700

    filter for race

--- os/controls/cerebellum.py
+++ os/controls/cerebellum.py
@@ -104,13 +104,14 @@ class Cerebellum(threading.Thread):
         self.prev_str = self.str
         self.state['prev_angles'] = self.state['angles']
 
-        if 50 < scaled_angle < 60:
-            if 50 <= self.prev_thr <= 60:
-                self.thr += 0.5
-            else:
-                self.thr = 50
-        else:
-            self.thr = 48
+        # if 50 < scaled_angle < 60:
+        #     if 50 <= self.prev_thr <= 60:
+        #         self.thr += 0.5
+        #     else:
+        #         self.thr = 50
+        # else:
+        #     self.thr = 48
+
 
 
 
