fcf16defc61def73d5fcccfa7a4a5c183eba2e5e
g-arnav <arnavbrookvale@gmail.com>
2019-06-29 12:25:14 -0700

    Added steering history

--- os/controls/cerebellum.py
+++ os/controls/cerebellum.py
@@ -106,7 +106,7 @@ class Cerebellum(threading.Thread):
             del self.angle_list[0]
         self.angle_list.append(scaled_angle_current)
 
-        scaled_angle = 0.8*scaled_angle_current+0.2*scaled_angle_history_avg
+        scaled_angle = 0.9*scaled_angle_current+0.1*scaled_angle_history_avg
 
         # Detecting which side of steer
         if 45 < scaled_angle < 65:
