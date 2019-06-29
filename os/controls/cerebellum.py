11deda632d4d43949aea251e63bfefd07bb24f94
g-arnav <arnavbrookvale@gmail.com>
2019-06-29 12:52:26 -0700

    Added steering history

--- os/controls/cerebellum.py
+++ os/controls/cerebellum.py
@@ -54,7 +54,7 @@ class Cerebellum(threading.Thread):
         self.state['z_accel']   = None
         self.state['prev_angles'] = self.state['angles']
 
-        self.angle_cnt_max = 20
+        self.angle_cnt_max = 10
         self.angle_list = [0]
 
     def update_state(self):
