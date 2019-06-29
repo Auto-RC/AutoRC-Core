551bc7ec8ec3c82ae15b84957cb5efefcc466163
g-arnav <arnavbrookvale@gmail.com>
2019-06-29 13:40:00 -0700

    Added steering history

--- os/controls/cerebellum.py
+++ os/controls/cerebellum.py
@@ -139,14 +139,14 @@ class Cerebellum(threading.Thread):
 
         # If straightaway then speed up
         if 50 < scaled_angle_current < 60:
-            if 80 <= self.prev_thr <= 90:
-                self.thr += 5
-            else: # Start at this value
-                self.thr = 80
+            # if 80 <= self.prev_thr <= 90:
+            #     self.thr += 5
+            # else: # Start at this value
+            self.thr = 80
         else:
             # Throttle based on turns
             if -45 < avg_angle < 45:
-                self.thr = 80 - (abs(avg_angle/90))*(80-45)
+                self.thr = 70 - (abs(avg_angle/90))*(70-45)
             else:
                 self.thr = 60 - (abs(avg_angle / 90)) * (60 - 45)
             # self.thr = 52
