3173d8de4400537eaef5b14fac4d7806063eb8c0
g-arnav <arnavbrookvale@gmail.com>
2019-09-21 12:06:50 -0700

    integrating

--- autorc/vehicle/vision/retina.py
+++ autorc/vehicle/vision/retina.py
@@ -341,7 +341,7 @@ class Retina():
         if 'Darwin' in platform.platform():
             self.contours = cv2.findContours(self.frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[1]
         else:
-            self.contours = cv2.findContours(self.frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[0]
+            self.contours = cv2.findContours(self.frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[1]
 
 
         # if self.mode == 'HSV':
