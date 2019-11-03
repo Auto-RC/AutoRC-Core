52ee46d6f164b74b21489f2fcd2fd28f85a452a4
embryo <a22agarw@outlook.com>
2019-11-03 08:49:32 -0800

    Fixed model name

--- autorc/vehicle/run.py
+++ autorc/vehicle/run.py
@@ -65,7 +65,7 @@ class AutoRC(threading.Thread):
         self.cortex = CortexSelect("ADVANCED", update_interval_ms=50, controller=self.controller, oculus=self.oculus, corti=self.corti)
         self.cortex.start()
 
-        self.cerebellum = CerebellumSelect("ADVANCED", update_interval_ms=50, controller=self.controller, cortex=self.cortex, corti=self.corti, model_name="One_Hot_Tests")
+        self.cerebellum = CerebellumSelect("ADVANCED", update_interval_ms=50, controller=self.controller, cortex=self.cortex, corti=self.corti, model_name="One_Hot_Test")
         self.cerebellum.start()
 
         self.drive = Drive(cerebellum=self.cerebellum, pca9685=self.pca9685,  update_interval_ms=10)
